62fc48711a49d4dc88a4f151780a8e526bb893da
Santiago Zapata <dev.santizapata@gmail.com>
2025-12-24 11:14:18 -0500

    Make: when display in page, make hide when click again

--- js/repository.js
+++ js/repository.js
@@ -5,6 +5,12 @@ function hide_show(button_id,elements_class){
     if (button.textContent === "See more ..."){
         for (let i = 0; i < hide_elements.length; i++){
             hide_elements[i].style.display = "block"
+            button.textContent = "See less ..."
+        }
+    } else if (button.textContent === "See less ..."){
+        for (let i = 0; i < hide_elements.length; i++){
+            hide_elements[i].style.display = "none"
+            button.textContent = "See more ..."
         }
     }
 }
